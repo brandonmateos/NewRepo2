9c80bf26ee01ffa867759147bc5828b04e0e4bec
Fernando Galvan <wiicoopp@gmail.com>
2019-10-31 11:55:13 -0600

    p,

---
+++ calculadora chida/calculadora chida/calculadora chida.cpp
@@ -0,0 +1,66 @@
+
+#include<iostream>
+#include<string>
+#include<cstdlib>
+
+using namespace std;
+void menu();
+int suma();
+int resta();
+int multi();
+int division();
+void pausa();
+
+int main()
+{
+	menu();
+	return 0;
+
+}
+//Creamos funciones
+void menu()
+{
+	bool bandera = false;
+	char tecla;
+	do
+	{
+		system("cls");
+		cin.clear();
+		cout << "Calculadora de Funciones" << endl;
+		cout << "------------------------" << endl;
+		cout << "\t1.- Sumar" << endl;
+		cout << "\t2.- Restar" << endl;
+		cout << "\t3.- Multiplicar" << endl;
+		cout << "\t4.- Dividir" << endl;
+		cout << "\t5.- Salir" << endl;
+		cout << "Elige una opcion: ";
+		cin >> tecla;
+		switch (tecla)
+		{
+		case'1':
+			system("cls");
+			suma();
+			break;
+		case'2':
+			system("cls");
+			resta();
+			break;
+		case'3':
+			system("cls");
+			multi();
+			break;
+		case'4':
+			system("cls");
+			division();
+			break;
+		case'5':
+			bandera = true;
+			break;
+		default:
+			system("cls");
+			cout << "Opcion no valida" << endl;
+			pausa();
+			break;
+		}
+	} while (bandera != true);
+}
